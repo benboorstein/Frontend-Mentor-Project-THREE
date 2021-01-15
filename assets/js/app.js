cfcff0f624a5278674ea93f005b4b96f07dd394e
Ben <benboorstein@gmail.com>
2021-01-15 12:09:59 -0500

    html/css changes for buttons/links accessibility.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,8 @@
 This should all apply the behavior of visually hiding the menu on mobile devices, showing it when the hamburger icon is activated, and ensuring the menu is fully visible on page load in wider screen sizes.
 
 */
+
+
+
+// Remember to make hamburger icon show menu ON HOVER, not on click.
+// if i need help: https://www.w3schools.com/howto/howto_css_dropdown.asp
