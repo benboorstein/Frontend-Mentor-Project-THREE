f47aba97efd7aef699ed79dc505c5110cf25da06
Ben <benboorstein@gmail.com>
2021-01-16 13:04:20 -0500

    addition of JS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,45 @@
-/* LEFT OFF HERE:
+const menuState = [
+    { // menu visible and X
+        menuItems: 'visible',
+        menuIcon: './assets/img/icon-close.svg',
+        menuIconWidth: '23px',
+        menuIconHeight: '23px'
+    },
+    { // menu hidden and hamburger
+        menuItems: 'hidden',
+        menuIcon: './assets/img/icon-hamburger.svg',
+        menuIconWidth: '29px',
+        menuIconHeight: '19.3px'
+    }
+]
 
-1. You'll use a combination of pre-setting up CSS classes and toggling them via JavaScript.
-This should all apply the behavior of visually hiding the menu on mobile devices, showing it when the hamburger icon is activated, and ensuring the menu is fully visible on page load in wider screen sizes.
+function menuShow(list) { // Functions should ALWAYS have inputs and expect outputs, and functions should behave exactly the same way every time they're run. And remember that what are passed in as parameters are "the things on which the function will operate".
+    document.querySelector('.menu-items').style.visibility = list[0].menuItems // QQQ: Should I just change the class "menu-items" to an id "menuItems" in HTML, CSS, and JS?
+    document.querySelector('.menu-three-lines').src = list[0].menuIcon // QQQ: Refer to "header button:active" QQQ in CSS regarding outline/border around the X.
+    document.querySelector('.menu-three-lines').style.width = list[0].menuIconWidth
+    document.querySelector('.menu-three-lines').style.height = list[0].menuIconHeight
+}
 
-*/
+function menuHide(list) {
+    document.querySelector('.menu-items').style.visibility = list[1].menuItems
+    document.querySelector('.menu-three-lines').src = list[1].menuIcon
+    document.querySelector('.menu-three-lines').style.width = list[1].menuIconWidth
+    document.querySelector('.menu-three-lines').style.height = list[1].menuIconHeight
+}
 
+document.getElementById('menuThreeLines').addEventListener('click', getMenuState)
 
+// ***** I think below is the problem area (and the only thing I can think is that maybe function calls can't be used this way). I could be wrong of course. ... On another note: I know it's not best to end an if...else statement with an "else if", but for now I'm just trying to make it work, and then I'll worry about correcting that part. *****
+function getMenuState() {
 
-// Remember to make hamburger icon show menu ON HOVER, not on click.
-// if i need help: https://www.w3schools.com/howto/howto_css_dropdown.asp
+    let x = menuHide(menuState)
+    let y = menuShow(menuState)
+
+    if (x.style.display === 'block') {
+        x.style.display === 'none'
+        y.style.display === 'block'
+    } else if (y.style.display === 'block') {
+        y.style.display === 'none'
+        x.style.display === 'block'
+    }
+}
